c3e13a9104641fd912bb4f8bd6176115c5c42cc3
Jack K <kwong999999@gmail.com>
2014-06-14 04:18:39 +0800

    moved beacon event generation code into its own package and added some test cases

--- app/BES/BES.js
+++ app/BES/BES.js
@@ -2,114 +2,6 @@ var FIREBASE_URL = 'https://beacon-event-sim.firebaseio.com/';
 
 
 
-/**
-By seconds stayed in proximity with a beacon before leaving, calculate number of "didRangeBeacons" and "didExitRegion" events to emit
-
-seconds_stayed : seconds stayed in proximity with a beacon before leaving
-
-return : a json specifying number of "didRangeBeacons" and "didExitRegion" should be emitted
-*/
-function calculateNumberOfRangeEventToSend(seconds_stayed){
-
-    var t = seconds_stayed;
-
-    if (t <= 0) {
-        return {"didRangeBeacons": 0, "didExitRegion": 0};
-    }else if(t > 0 && t < 2){
-        return {"didRangeBeacons": 1, "didExitRegion": 1};
-    }else if(t >= 2 && t < 3){
-        return {"didRangeBeacons": 2, "didExitRegion": 1};
-    }else if(t >= 3 && t < 4){
-        return {"didRangeBeacons": 3, "didExitRegion": 1};
-    }else if(t >= 4 && t < 5){
-        return {"didRangeBeacons": 4, "didExitRegion": 1};
-    }else{ // t>= 5
-        return {"didRangeBeacons": 5, "didExitRegion": 1};
-    }
-
-}
-
-/**
-Process simulation seed to generate each beacon event, and pass each beacon event to user-defined handler
-
-json : the simulation seed that specify what beacon events to generate
-beacon_event_handler : function with signature "function(beacon_event)", get called when a beacon event is available
-
-return : nothing
-*/
-function processSimulationSeed(json, beacon_event_handler){
-
-    var beacons = json["setup"]["beacons"];
-    var mobiles = json["setup"]["mobiles"];
-
-    var beacon_mobile_events = json["events_between"];
-
-    _.each( _.keys(beacon_mobile_events), function(beacon_alias){
-
-        var beacon = beacons[beacon_alias];
-        var mobile_events = beacon_mobile_events[beacon_alias];
-
-        _.each( _.keys(mobile_events), function(mobile_alias){
-
-            var mobile = mobiles[mobile_alias];
-            var events = mobile_events[mobile_alias];
-
-            _.each( events, function(event){
-
-                var seconds_stayed = event["seconds_stayed"];
-                var start_time = event["start_time"];
-                var major = beacon["major"];
-                var minor = beacon["minor"];
-                var uuid = beacon["uuid"];
-                var visitor_uuid = mobile["uuid"];
-
-                var start_time_ms = Date.parse(start_time);
-                var number_of_events_to_send = calculateNumberOfRangeEventToSend(seconds_stayed);
-
-                var one_second_in_ms = 1000;
-
-                for (var i = 0; i < number_of_events_to_send["didRangeBeacons"]; i++) {
-                    var created_at = new Date (start_time_ms + i * one_second_in_ms);
-                    var beacon_event = {
-                        "type" : "didRangeBeacons",
-                        "minor" : minor,
-                        "major" : major,
-                        "uuid" : uuid,
-                        "visitor_uuid" : visitor_uuid,
-                        "created_at" : created_at.toString()
-                    };
-                    beacon_event_handler(beacon_event);
-                };
-
-                if (number_of_events_to_send["didExitRegion"] > 0) {
-                    var created_at = new Date (start_time_ms + seconds_stayed * one_second_in_ms);
-                    var beacon_event = {
-                        "type" : "didExitRegion",
-                        "minor" : minor,
-                        "major" : major,
-                        "uuid" : uuid,
-                        "visitor_uuid" : visitor_uuid,
-                        "created_at" : created_at.toString()
-                    };
-                    beacon_event_handler(beacon_event);
-                };
-
-
-
-
-            } );
-
-        } );
-
-    } );
-
-}
-
-
-
-
-
-
 
 if (Meteor.isClient) {
 
@@ -146,7 +38,7 @@ if (Meteor.isClient) {
 
                         var firebase_upload_log = '';
 
-                        processSimulationSeed(json, function(beacon_event){ 
+                        Beg.processSimulationSeed(json, function(beacon_event){ 
                             firebase.push(beacon_event);
                             firebase_upload_log += '****Sent Beacon Event:\n';
                             firebase_upload_log += JSON.stringify(beacon_event, null, 4);
@@ -212,7 +104,7 @@ if (Meteor.isServer) {
                         //Use the firebase from NPM
                         var firebase_npm = new Firebase(FIREBASE_URL);
 
-                        processSimulationSeed(json, function(beacon_event){
+                        Beg.processSimulationSeed(json, function(beacon_event){
                             firebase_npm.push().set(beacon_event);
                             console.log('****Sent Beacon Event:');
                             console.log( JSON.stringify(beacon_event, null, 4), '\n');
